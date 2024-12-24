4d249b1127ee0c1969d1890f48ecc05bfa0ef8bf
Sujan Tamang <sujanprogrammer@gmail.com>
2024-12-24 09:30:40 +0545

    V-2

--- portfolio/porfolio/settings.py
+++ portfolio/porfolio/settings.py
@@ -74,14 +74,23 @@ WSGI_APPLICATION = 'porfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.oracle",
+        "NAME": "sujan",
+        "USER": "postgresql",
+        "PASSWORD": "sujan@789",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
